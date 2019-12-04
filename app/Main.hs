b5e872d6c5aeb0c4f3850b39876d03641184f2c4
Luminiscental <luminiscental01@gmail.com>
2019-12-04 21:05:44 +0000

    Pick board dimensions based on terminal size when possible

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,7 @@ import           Data.Set                       ( fromList )
 import           Data.Maybe                     ( fromMaybe )
 
 -- TODO: Commandline options to specify starting board and other parameters
-
 main :: IO ()
 main =
     let bounds = Bounds { minX = -10, minY = -5, maxX = 10, maxY = 5 }
-    in  displayGame bounds . fromMaybe emptyBoard . getNamedBoard $ "glider"
+    in  animateGame bounds . fromMaybe emptyBoard . getNamedBoard $ "glider"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,25 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
+{-|
+Module      : Lib
+Description : The business library for the game.
+Maintainer  : luminiscental01@gmail.com
+
+The main class defined is the 'Board' class representing a static state of the game,
+with 'animateGame' allowing its evoluation over time to be visualized on a command line.
+-}
+
 module Lib
     ( Board(..)
     , Bounds(..)
     , neighbourGrid
     , countNeighbours
-    , survives
+    , aliveNext
     , stepBoard
-    , playGame
+    , generations
     , getBounds
     , displayBoard
-    , displayGame
+    , animateGame
     , emptyBoard
     , getNamedBoard
     , translateBoard
@@ -23,6 +32,7 @@ import qualified Data.Map.Strict               as M
 import qualified Data.Set                      as S
 import           Data.Maybe                     ( fromMaybe )
 import           Data.List                      ( intersperse )
+import           Data.Tuple                     ( swap )
 import           Control.Monad                  ( guard
                                                 , unless
                                                 )
@@ -30,14 +40,22 @@ import           System.Console.ANSI            ( clearScreen
                                                 , hideCursor
                                                 , showCursor
                                                 , setCursorPosition
+                                                , getTerminalSize
                                                 )
 import           System.IO                      ( stdin
                                                 , hWaitForInput
                                                 )
 
+-- | The 'Board' type represents a static board for the game of life.
+-- It is a set of active cells represented by their cartesian coordinates.
 type Board = Set (Int, Int)
 
-data Bounds = Bounds { minX :: Int, minY :: Int, maxX :: Int, maxY :: Int }
+-- | The 'Bounds' type represents a rectangular region of the grid.
+data Bounds = Bounds { minX :: Int -- ^ The minimum x coordinate of the grid (inclusive).
+                     , minY :: Int -- ^ The minimum y coordinate of the grid (inclusive).
+                     , maxX :: Int -- ^ The maximum x coordinate of the grid (inclusive).
+                     , maxY :: Int -- ^ The maximum y coordinate of the grid (inclusive).
+                     }
 
 boundWidth :: Bounds -> Int
 boundWidth b = maxX b - minX b
@@ -45,6 +63,7 @@ boundWidth b = maxX b - minX b
 boundHeight :: Bounds -> Int
 boundHeight b = maxY b - minY b
 
+-- | Extend a region of the grid by growing in every direction.
 extendBounds :: Bounds -> Bounds
 extendBounds b = Bounds { minX = minX b - 1
                         , minY = minY b - 1
@@ -52,21 +71,25 @@ extendBounds b = Bounds { minX = minX b - 1
                         , maxY = maxY b + 1
                         }
 
-emptyBounds :: (Int, Int) -> Bounds
-emptyBounds (x, y) = Bounds { minX = x, minY = y, maxX = x, maxY = y }
+-- | Create a region containing a single point.
+unitBounds :: (Int, Int) -> Bounds
+unitBounds (x, y) = Bounds { minX = x, minY = y, maxX = x, maxY = y }
 
+-- | Get a lit of all points in a region.
 gridRange :: Bounds -> [(Int, Int)]
 gridRange b = do
     x <- [minX b .. maxX b]
     y <- [minY b .. maxY b]
     return (x, y)
 
+-- | Get a list of points neighbour to a given point (not including the original point).
 neighbourGrid :: (Int, Int) -> [(Int, Int)]
 neighbourGrid (x, y) = do
-    (x', y') <- gridRange . extendBounds . emptyBounds $ (x, y)
+    (x', y') <- gridRange . extendBounds . unitBounds $ (x, y)
     guard (x' /= x || y' /= y)
     return (x', y')
 
+-- | Count the number of living neighbours to a cell in the board.
 countNeighbours :: Board -> (Int, Int) -> Int
 countNeighbours board position = length liveIndicators
   where
@@ -74,21 +97,26 @@ countNeighbours board position = length liveIndicators
     indicatorList  = map (`S.member` board) grid
     grid           = neighbourGrid position
 
-survives :: Board -> (Int, Int) -> Bool
-survives board position =
+-- | Check whether a cell in the board will be alive next turn.
+aliveNext :: Board -> (Int, Int) -> Bool
+aliveNext board position =
     let neighbours = countNeighbours board position
         alive      = S.member position board
     in  (neighbours == 3) || (alive && neighbours == 2)
 
+-- | Calculate the next generation of a board.
 stepBoard :: Board -> Board
 stepBoard board =
     let cellsToConsider =
                 S.fromList . gridRange . extendBounds . getBounds $ board
-    in  S.filter (survives board) cellsToConsider
+    in  S.filter (aliveNext board) cellsToConsider
 
-playGame :: Board -> [Board]
-playGame board = board : playGame (stepBoard board)
+-- | Given an initial board state returns an infinite list of the following generations.
+generations :: Board -> [Board]
+generations board = board : generations (stepBoard board)
 
+-- | Returns the smallest region containing all living cells in the board,
+-- or just the origin for an empty board.
 getBounds :: Board -> Bounds
 getBounds board =
     let cellXs = S.map fst board
@@ -108,20 +136,22 @@ blinker = S.fromList [(0, 0), (1, 0), (2, 0)]
 toad :: Board
 toad = S.fromList [(0, 0), (1, 0), (2, 0), (1, 1), (2, 1), (3, 1)]
 
+-- | Create a board with no live cells.
 emptyBoard :: Board
 emptyBoard = S.empty
 
+-- | A map of board presets which have special names.
 namedBoards :: Map String Board
 namedBoards =
     M.fromList [("glider", glider), ("blinker", blinker), ("toad", toad)]
 
+-- | Get a board preset from its name.
 getNamedBoard :: String -> Maybe Board
 getNamedBoard = flip M.lookup namedBoards
 
 -- TODO: parseBoard :: Parser Board for command line input of initial board
 
--- TODO: Make axis/origin positions clear
--- TODO: Dynamic resizing to cover all living cells maybe?
+-- | Display the cells of the board in a given region as a string.
 displayBoard :: Bounds -> Board -> String
 displayBoard bounds board =
     let displayCell cell = if S.member cell board then '■' else ' '
@@ -130,6 +160,10 @@ displayBoard bounds board =
         grid = map displayRow [minY bounds .. maxY bounds]
     in  displayGrid '|' '-' grid
 
+-- | Easiest described by example:
+--
+-- >>> extrasperse '|' "123abc"
+-- "|1|2|3|a|b|c|"
 extrasperse :: a -> [a] -> [a]
 extrasperse x xs = x : intersperse x xs ++ [x]
 
@@ -140,13 +174,40 @@ displayGrid colSep rowSep grid =
         displayRow = extrasperse colSep
     in  unlines . extrasperse rowLine . map displayRow $ grid
 
+-- | Shift every cell of the board by a given change in x and y.
 translateBoard :: Int -> Int -> Board -> Board
 translateBoard dx dy = S.map $ \(x, y) -> (x + dx, y + dy)
 
--- TODO: Parametrize options in some reader monad
-displayGame :: Bounds -> Board -> IO ()
-displayGame bounds board =
-    let game        = playGame board
+conservativeHalve :: Int -> Int
+conservativeHalve n = (n - 1) `div` 2
+
+-- | Create a centered bounding region from given dimensions.
+-- If a dimension is even the resulting bounds will have the next smallest
+-- corresponding odd dimension.
+centeredBounds :: (Int, Int) -> Bounds
+centeredBounds (width, height) =
+    let halfW = conservativeHalve width
+        halfH = conservativeHalve height
+    in  Bounds { minX = -halfW, minY = -halfH, maxX = halfW, maxY = halfH }
+
+-- | Adjust terminal dimensions to board dimensions to account for row/column separators.
+adjustDimensions :: (Int, Int) -> (Int, Int)
+adjustDimensions (x, y) = (conservativeHalve x, conservativeHalve y)
+
+-- TODO: Allow input to translate the view
+-- TODO: Parametrize options
+-- | Animate the generations of a given initial board state on the command line,
+-- with the option to exit by pressing enter.
+animateGame
+    :: Bounds -- ^ The default bounds to use if terminal size can't be found from 'getTerminalSize'.
+    -> Board -- ^ The initial board state to animate
+    -> IO ()
+animateGame defaultBounds board = do
+    terminalSize <- getTerminalSize
+    let bounds = maybe defaultBounds
+                       (centeredBounds . adjustDimensions . swap)
+                       terminalSize
+        game        = generations board
         screens     = map (putStrLn . displayBoard bounds) game
         title       = "<Press enter to exit>"
         frameTimeMS = 500
@@ -158,7 +219,6 @@ displayGame bounds board =
             screen
             pressed <- hWaitForInput stdin frameTimeMS
             unless pressed $ run rest
-    in  do
-            hideCursor
-            run screens
-            showCursor
+    hideCursor
+    run screens
+    showCursor
